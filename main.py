3c4bebc8c047ade9fbe797697cd944e4feb6b5f5
Eugene Vyshnytsky <101946519+Vishnyak13@users.noreply.github.com>
2022-10-26 13:57:29 +0300

    added thread and process

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import concurrent.futures
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from multiprocessing import Process
+from multiprocessing.dummy import Pool
 from time import time, sleep
 from threading import Thread
 
@@ -19,7 +21,16 @@ if __name__ == '__main__':
     timer = time()
     a, b, c, d = factorize(128, 255, 99999, 10651060)
     print(a, b, c, d, sep='\n')
-    print(f'Main thread python done in {time() - timer:.2f} seconds')
+    print(f'Main thread python done in {time() - timer:.3f} seconds')
+    sleep(1)
+
+    # test with 1 process
+    timer_1 = time()
+    p = Process(target=factorize, args=(128, 255, 99999, 10651060))
+    p.start()
+    p.join()
+    print(f'Test 1 process done in {time() - timer_1:.3f} seconds')
+    sleep(1)
 
     # test with threading in 4 threads
     timer_2 = time()
@@ -29,17 +40,27 @@ if __name__ == '__main__':
         threads.append(thread)
         thread.start()
     [thread.join() for thread in threads]
-    print(f'Test with threading in 4 threads done in {time() - timer_2:.2f} seconds')
+    print(f'Test with threading in 4 threads done in {time() - timer_2:.3f} seconds')
+    sleep(1)
 
     # test with TreadPoolExecutor with 4 threads
     timer_3 = time()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+    with ThreadPoolExecutor(max_workers=4) as executor:
         executor.map(factorize, (128, 255, 99999, 10651060))
-    print(f'Test with TreadPoolExecutor with 4 threads done in {time() - timer_3:.2f} seconds')
+    print(f'Test with TreadPoolExecutor with 4 threads done in {time() - timer_3:.3f} seconds')
+    sleep(1)
 
-    # test with ProcessPoolExecutor with 4 threads
+    # test with ProcessPoolExecutor with 4 processes
     timer_4 = time()
-    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+    with ProcessPoolExecutor(max_workers=4) as executor:
         executor.map(factorize, (128, 255, 99999, 10651060))
-    print(f'Test with ProcessPoolExecutor with 4 threads done in {time() - timer_4:.2f} seconds')
+    print(f'Test with ProcessPoolExecutor with 4 processes done in {time() - timer_4:.3f} seconds')
+    sleep(1)
+
+    # test with Pool with 4 processes
+    timer_5 = time()
+    with Pool(4) as pool:
+        pool.map(factorize, (128, 255, 99999, 10651060))
+    print(f'Test with Pool with 4 processes done in {time() - timer_5:.3f} seconds')
+
 
